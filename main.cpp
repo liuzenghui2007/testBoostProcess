01c438fd2af06c0e7e398826735742475728378c
lzh <240164005@qq.com>
2023-10-25 15:18:44 +0800

    tofile

--- main.cpp
+++ main.cpp
@@ -5,35 +5,38 @@
 #include <thread>
 #include <csignal>
 #include <windows.h>
-#include <iomanip>  // 用于格式化日期和时间
+#include <iomanip>
+#include <fstream>  // 添加文件流操作头文件
 
-volatile sig_atomic_t pythonExited = false;
+volatile sig_atomic_t processExited = false;
 
-void pythonExitHandler(int /* signum */) {
-    pythonExited = true;
+void processExitHandler(int /* signum */) {
+    processExited = true;
 }
 
 int main() {
-    // 设置Python脚本退出信号处理函数
-    signal(SIGTERM, pythonExitHandler);
+    // 设置process脚本退出信号处理函数
+    signal(SIGTERM, processExitHandler);
+
+    auto now = std::chrono::system_clock::now();
+    auto now_c = std::chrono::system_clock::to_time_t(now);
+    struct tm timeinfo;
+    localtime_s(&timeinfo, &now_c);
+    char time_str[20];
+    strftime(time_str, sizeof(time_str), "%Y-%m-%d %H-%M-%S", &timeinfo);
+
+    // 创建文件并打开以写入内容
+    std::ofstream outputFile(time_str + std::string(".txt"));
 
     while (true) {
         auto start_time = std::chrono::high_resolution_clock::now();
-        std::string cmd = "C://Users//24016//miniconda3//python.exe d://random_run.py";
+        std::string cmd = "./test.exe";
         boost::process::child c(cmd, boost::process::std_out > boost::process::null, boost::process::std_err > boost::process::null);
 
-        // 获取当前时间并格式化为带有年月日时分秒的字符串
-        auto now = std::chrono::system_clock::now();  // 使用系统时钟
-        auto now_c = std::chrono::system_clock::to_time_t(now);  // 转换为time_t
-        struct tm timeinfo;
-        localtime_s(&timeinfo, &now_c);
-        char time_str[20];
-        strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", &timeinfo);
-
         // 打印启动时间、进程ID和PID
-        std::cout << "Started Python script with PID: " << c.id() << " at " << time_str << std::endl;
+        outputFile << "Started process with PID: " << c.id() << " at " << time_str << std::endl;
 
-        // 等待直到Python脚本退出
+        // 等待直到process脚本退出
         while (true) {
             DWORD exitCode;
             if (GetExitCodeProcess(c.native_handle(), &exitCode)) {
@@ -44,19 +47,23 @@ int main() {
             std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
 
-        // Python脚本退出后的处理
+        // process脚本退出后的处理
         auto end_time = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time);
-        std::cout << "Python script ran for " << duration.count() << " seconds." << std::endl;
+        outputFile << "process ran for " << duration.count() << " seconds." << std::endl;
 
-        // 检查Python脚本是否已退出
-        if (pythonExited) {
-            std::cout << "Python script has exited." << std::endl;
+        // 检查process脚本是否已退出
+        if (processExited) {
+            outputFile << "process has exited." << std::endl;
         }
+
+
 
         // 暂停一段时间，然后继续下一轮循环
         std::this_thread::sleep_for(std::chrono::seconds(5)); // 为了演示目的，这里暂停5秒
     }
 
+    // 关闭文件
+    outputFile.close();
     return 0;
 }
